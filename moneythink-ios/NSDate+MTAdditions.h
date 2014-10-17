b8071a22e58db7adc84ef712a6f53effbe26f938
David W Sica <david@sufficientmagic.com>
2014-10-16 18:08:53 -0600

    Changes to support MTIOS-105, MTIOS-114, and MTIOS-129

---
+++ moneythink-ios/NSDate+MTAdditions.h
@@ -0,0 +1,13 @@
+//
+//  NSDate+MTAdditions.h
+//  moneythink-ios
+//
+//  Created by David Sica on 10/13/14.
+//  Copyright (c) 2014 Moneythink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (MTAdditions)
+
+@end
---
+++ moneythink-ios/NSDate+MTAdditions.m
@@ -0,0 +1,13 @@
+//
+//  NSDate+MTAdditions.m
+//  moneythink-ios
+//
+//  Created by David Sica on 10/13/14.
+//  Copyright (c) 2014 Moneythink. All rights reserved.
+//
+
+#import "NSDate+MTAdditions.h"
+
+@implementation NSDate (MTAdditions)
+
+@end
