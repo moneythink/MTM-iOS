51781d033b7bd61437b366527768ace9b889d3ee
David W Sica <david@sufficientmagic.com>
2014-11-04 11:47:52 -0700

    Remove login screen prompt and show future challenges properly

--- moneythink-ios/MTChallengesContentViewController.m
+++ moneythink-ios/MTChallengesContentViewController.m
@@ -24,8 +24,11 @@
 
 @property (strong, nonatomic) IBOutlet UIView *leftPanel;
 @property (strong, nonatomic) IBOutlet UIView *rightPanel;
+@property (nonatomic, strong) IBOutlet UIView *separatorView;
 
 @property (assign, nonatomic) BOOL activated;
+@property (nonatomic) BOOL openChallenge;
+@property (nonatomic) BOOL queriedForOpenness;
 
 @end
 
@@ -52,33 +55,15 @@
                                    action:@selector(exploreChallenge)];
     
     self.activated = NO;
-    
     [self.view addGestureRecognizer:tap];
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
+    [super viewWillAppear:animated];
+    
     NSString *userClass = [PFUser currentUser][@"class"];
     NSString *userSchool = [PFUser currentUser][@"school"];
-    
-    NSPredicate *challengePredicate = [NSPredicate predicateWithFormat:@"challenge_number = %@", self.challenge[@"challenge_number"]];
-    PFQuery *checkSchedule = [PFQuery queryWithClassName:[PFScheduledActivations parseClassName] predicate:challengePredicate];
-    
-    checkSchedule.cachePolicy = kPFCachePolicyNetworkElseCache;
-    
-    [checkSchedule whereKey:@"activated" equalTo:@YES];
-    [checkSchedule whereKey:@"class" equalTo:userClass];
-    [checkSchedule whereKey:@"school" equalTo:userSchool];
-    
-    [checkSchedule countObjectsInBackgroundWithBlock:^(int number, NSError *error) {
-        self.challengeState.text = (number > 0) ? @"OPEN CHALLENGE" : @"FUTURE CHALLENGE";
-
-        if ([[PFUser currentUser][@"type"] isEqualToString:@"mentor"]) {
-            self.activated = YES;
-        } else {
-            self.activated = number > 0;
-        }
-    }];
     
     self.challengeState.text = self.challengeStateText;
     self.challengeTitle.text = self.challengeTitleText;
@@ -86,26 +71,131 @@
     self.challengeDescription.text = self.challengeDescriptionText;
     self.challengePoints.text = [self.challengePointsText stringByAppendingString:@" pts"];
     
-    NSString *pillar = self.challengePillarText;
-    
-    if ([pillar isEqualToString:@"Money Manager"]) {
-        self.leftPanel.backgroundColor = [UIColor mutedOrange];
-        self.rightPanel.backgroundColor = [UIColor primaryOrange];
-    } else {
-        self.leftPanel.backgroundColor = [UIColor mutedGreen];
-        self.rightPanel.backgroundColor = [UIColor primaryGreen];
-    }
-
+    __block NSString *pillar = self.challengePillarText;
     self.rightPanel.layer.cornerRadius = 4.0f;
+
+    // Pull challenge info
+    if (!self.queriedForOpenness) {
+        MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:[UIApplication sharedApplication].keyWindow animated:YES];
+        hud.labelText = @"Loading...";
+        hud.dimBackground = YES;
+        
+        self.challengeState.alpha = 0.0f;
+        self.rightPanel.alpha = 0.0f;
+        self.challengeIcon.alpha = 0.0f;
+    }
+    else {
+        self.challengeState.alpha = 1.0f;
+        self.rightPanel.alpha = 1.0f;
+        self.challengeIcon.alpha = 1.0f;
+        
+        if (self.openChallenge) {
+            self.challengeState.text = @"OPEN CHALLENGE";
+            self.challengeIcon.image = [UIImage imageNamed:@"icon_challenge_open"];
+            self.separatorView.alpha = 0.0f;
+        }
+        else {
+            self.challengeState.text = @"FUTURE CHALLENGE";
+            self.challengeIcon.image = [UIImage imageNamed:@"icon_challenge_future"];
+            self.separatorView.alpha = 1.0f;
+        }
+    }
+    
+    MTMakeWeakSelf();
+    [self bk_performBlock:^(id obj) {
+        NSPredicate *challengePredicate = [NSPredicate predicateWithFormat:@"challenge_number = %@", weakSelf.challenge[@"challenge_number"]];
+        PFQuery *checkSchedule = [PFQuery queryWithClassName:[PFScheduledActivations parseClassName] predicate:challengePredicate];
+        
+        if (!weakSelf.queriedForOpenness) {
+            checkSchedule.cachePolicy = kPFCachePolicyNetworkElseCache;
+        }
+        else {
+            checkSchedule.cachePolicy = kPFCachePolicyNetworkOnly;
+        }
+
+        [checkSchedule whereKey:@"activated" equalTo:@YES];
+        [checkSchedule whereKey:@"class" equalTo:userClass];
+        [checkSchedule whereKey:@"school" equalTo:userSchool];
+        
+        [checkSchedule countObjectsInBackgroundWithBlock:^(int number, NSError *error) {
+            
+            weakSelf.queriedForOpenness = YES;
+            
+            if ([[PFUser currentUser][@"type"] isEqualToString:@"mentor"]) {
+                weakSelf.activated = YES;
+            } else {
+                weakSelf.activated = number > 0;
+            }
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [MBProgressHUD hideAllHUDsForView:[UIApplication sharedApplication].keyWindow animated:YES];
+                weakSelf.challengeState.alpha = 1.0f;
+                weakSelf.rightPanel.alpha = 1.0f;
+                weakSelf.challengeIcon.alpha = 1.0f;
+                
+                weakSelf.openChallenge = (number > 0);
+                if (weakSelf.openChallenge) {
+                    weakSelf.challengeState.text = @"OPEN CHALLENGE";
+                    weakSelf.challengeIcon.image = [UIImage imageNamed:@"icon_challenge_open"];
+                    weakSelf.separatorView.alpha = 0.0f;
+                }
+                else {
+                    weakSelf.challengeState.text = @"FUTURE CHALLENGE";
+                    weakSelf.challengeIcon.image = [UIImage imageNamed:@"icon_challenge_future"];
+                    weakSelf.separatorView.alpha = 1.0f;
+                }
+                
+                if ([pillar isEqualToString:@"Money Manager"]) {
+                    weakSelf.leftPanel.backgroundColor = [UIColor mutedOrange];
+                    weakSelf.rightPanel.layer.borderColor = [UIColor primaryOrange].CGColor;
+                    
+                    if (weakSelf.openChallenge) {
+                        weakSelf.rightPanel.backgroundColor = [UIColor primaryOrange];
+                        weakSelf.rightPanel.layer.borderWidth = 0.0f;
+                        weakSelf.challengeDescription.textColor = [UIColor whiteColor];
+                        weakSelf.challengeTitle.textColor = [UIColor whiteColor];
+                        weakSelf.challengePoints.textColor = [UIColor whiteColor];
+                    }
+                    else {
+                        weakSelf.rightPanel.backgroundColor = [UIColor clearColor];
+                        weakSelf.rightPanel.layer.borderWidth = 1.0f;
+                        weakSelf.challengeDescription.textColor = [UIColor primaryOrange];
+                        weakSelf.challengeTitle.textColor = [UIColor primaryOrange];
+                        weakSelf.challengePoints.textColor = [UIColor primaryOrange];
+                        weakSelf.separatorView.backgroundColor = [UIColor primaryOrange];
+                    }
+                    
+                } else {
+                    weakSelf.leftPanel.backgroundColor = [UIColor mutedGreen];
+                    weakSelf.rightPanel.layer.borderColor = [UIColor primaryGreen].CGColor;
+                    
+                    if (weakSelf.openChallenge) {
+                        weakSelf.rightPanel.backgroundColor = [UIColor primaryGreen];
+                        weakSelf.rightPanel.layer.borderWidth = 0.0f;
+                        weakSelf.challengeDescription.textColor = [UIColor whiteColor];
+                        weakSelf.challengeTitle.textColor = [UIColor whiteColor];
+                        weakSelf.challengePoints.textColor = [UIColor whiteColor];
+                    }
+                    else {
+                        weakSelf.rightPanel.backgroundColor = [UIColor clearColor];
+                        weakSelf.rightPanel.layer.borderWidth = 1.0f;
+                        weakSelf.challengeDescription.textColor = [UIColor primaryGreen];
+                        weakSelf.challengeTitle.textColor = [UIColor primaryGreen];
+                        weakSelf.challengePoints.textColor = [UIColor primaryGreen];
+                        weakSelf.separatorView.backgroundColor = [UIColor primaryGreen];
+                    }
+                }
+            });
+        }];
+    } afterDelay:0.35f];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)objectsReturned:(int)number error:(NSError *)error
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [MBProgressHUD hideAllHUDsForView:[UIApplication sharedApplication].keyWindow animated:YES];
+    });
 
-- (void)objectsReturned:(int)number {
     NSInteger count = number;
     NSString *type = [PFUser currentUser][@"type"];
     
@@ -124,30 +214,36 @@
     }
 }
 
--(void)exploreChallenge {
+-(void)exploreChallenge
+{
     if (self.activated) {
-        NSPredicate *challengePredicate = [NSPredicate predicateWithFormat:@"challenge_number = %@", self.challenge[@"challenge_number"]];
-        PFQuery *queryActivated = [PFQuery queryWithClassName:[PFChallengesActivated parseClassName] predicate:challengePredicate];
-        [queryActivated whereKeyDoesNotExist:@"school"];
-        [queryActivated whereKeyDoesNotExist:@"class"];
-        
-//        queryActivated.cachePolicy = kPFCachePolicyCacheThenNetwork;
-//        
-        [queryActivated countObjectsInBackgroundWithTarget:self selector:@selector(objectsReturned:)];
+        MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:[UIApplication sharedApplication].keyWindow animated:YES];
+        hud.labelText = @"Loading...";
+        hud.dimBackground = YES;
+
+        MTMakeWeakSelf();
+        [self bk_performBlock:^(id obj) {
+            NSPredicate *challengePredicate = [NSPredicate predicateWithFormat:@"challenge_number = %@", weakSelf.challenge[@"challenge_number"]];
+            PFQuery *queryActivated = [PFQuery queryWithClassName:[PFChallengesActivated parseClassName] predicate:challengePredicate];
+            [queryActivated whereKeyDoesNotExist:@"school"];
+            [queryActivated whereKeyDoesNotExist:@"class"];
+            
+            queryActivated.cachePolicy = kPFCachePolicyNetworkElseCache;
+            
+            [queryActivated countObjectsInBackgroundWithTarget:weakSelf selector:@selector(objectsReturned:error:)];
+        } afterDelay:0.35f];
     }
 }
 
 
 #pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-//    MTPostsTabBarViewController *destination = (MTPostsTabBarViewController *)[segue destinationViewController];
     MTStudentChallengeRoomViewController *destination = (MTStudentChallengeRoomViewController *)[segue destinationViewController];
     
     destination.challenge = self.challenge;
     destination.challengeNumber = self.challengeNumberText;
 }
+
 
 @end
