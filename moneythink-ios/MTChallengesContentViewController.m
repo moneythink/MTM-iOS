2ebe885e068ce387215efbe1d7f8b0a8639f1f58
David W Sica <david@sufficientmagic.com>
2014-11-07 13:13:33 -0700

    Fix for UITextView textColor not updating properly when swiping through challenge tiles.

--- moneythink-ios/MTChallengesContentViewController.m
+++ moneythink-ios/MTChallengesContentViewController.m
@@ -48,8 +48,11 @@
 {
     [super viewDidLoad];
     
-    self.challengeDescription.textColor = [UIColor white];
-    
+    self.challengeState.alpha = 0.0f;
+    self.rightPanel.alpha = 0.0f;
+    self.challengeIcon.alpha = 0.0f;
+    self.rightPanel.layer.cornerRadius = 4.0f;
+
     [self.activateButton addTarget:self action:@selector(exploreChallenge) forControlEvents:UIControlEventTouchUpInside];
     [self.activateButton setBackgroundImage:[UIImage imageWithColor:[UIColor colorWithWhite:0.0f alpha:0.4f] size:self.activateButton.frame.size] forState:UIControlStateHighlighted];
     [self.activateButton setBackgroundImage:[UIImage imageWithColor:[UIColor clearColor] size:self.activateButton.frame.size] forState:UIControlStateNormal];
@@ -61,6 +64,10 @@
 {
     [super viewWillAppear:animated];
     
+    self.challengeState.alpha = 0.0f;
+    self.rightPanel.alpha = 0.0f;
+    self.challengeIcon.alpha = 0.0f;
+    
     NSString *userClass = [PFUser currentUser][@"class"];
     NSString *userSchool = [PFUser currentUser][@"school"];
     
@@ -69,9 +76,6 @@
     self.challengeNumber.text = self.challengeNumberText;
     self.challengeDescription.text = self.challengeDescriptionText;
     self.challengePoints.text = [self.challengePointsText stringByAppendingString:@" pts"];
-    
-    __block NSString *pillar = self.challengePillarText;
-    self.rightPanel.layer.cornerRadius = 4.0f;
 
     // Pull challenge info
     if (!self.queriedForOpenness) {
@@ -80,16 +84,8 @@
         MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:[UIApplication sharedApplication].keyWindow animated:YES];
         hud.labelText = @"Refreshing Challenge...";
         hud.dimBackground = YES;
-        
-        self.challengeState.alpha = 0.0f;
-        self.rightPanel.alpha = 0.0f;
-        self.challengeIcon.alpha = 0.0f;
     }
     else {
-        self.challengeState.alpha = 1.0f;
-        self.rightPanel.alpha = 1.0f;
-        self.challengeIcon.alpha = 1.0f;
-        
         if (self.openChallenge) {
             self.challengeState.text = @"OPEN CHALLENGE";
             self.challengeIcon.image = [UIImage imageNamed:@"icon_challenge_open"];
@@ -101,9 +97,15 @@
             self.separatorView.alpha = 1.0f;
         }
     }
+
+    // Pre-configure cell before requesting updates if already queried
+    if (self.queriedForOpenness) {
+        [self configureContentViewForCurrentState];
+    }
     
     MTMakeWeakSelf();
     [self bk_performBlock:^(id obj) {
+
         NSPredicate *challengePredicate = [NSPredicate predicateWithFormat:@"challenge_number = %@", weakSelf.challenge[@"challenge_number"]];
         PFQuery *checkSchedule = [PFQuery queryWithClassName:[PFScheduledActivations parseClassName] predicate:challengePredicate];
         
@@ -120,75 +122,93 @@
         
         [checkSchedule countObjectsInBackgroundWithBlock:^(int number, NSError *error) {
             
-            weakSelf.queriedForOpenness = YES;
-            
-            if ([[PFUser currentUser][@"type"] isEqualToString:@"mentor"]) {
-                weakSelf.activated = YES;
-            } else {
-                weakSelf.activated = number > 0;
-            }
-            
             dispatch_async(dispatch_get_main_queue(), ^{
-                [MBProgressHUD hideAllHUDsForView:[UIApplication sharedApplication].keyWindow animated:YES];
-                weakSelf.challengeState.alpha = 1.0f;
-                weakSelf.rightPanel.alpha = 1.0f;
-                weakSelf.challengeIcon.alpha = 1.0f;
+
+                weakSelf.queriedForOpenness = YES;
                 
-                weakSelf.openChallenge = (number > 0);
-                if (weakSelf.openChallenge) {
-                    weakSelf.challengeState.text = @"OPEN CHALLENGE";
-                    weakSelf.challengeIcon.image = [UIImage imageNamed:@"icon_challenge_open"];
-                    weakSelf.separatorView.alpha = 0.0f;
-                }
-                else {
-                    weakSelf.challengeState.text = @"FUTURE CHALLENGE";
-                    weakSelf.challengeIcon.image = [UIImage imageNamed:@"icon_challenge_future"];
-                    weakSelf.separatorView.alpha = 1.0f;
-                }
-                
-                if ([pillar isEqualToString:@"Money Manager"]) {
-                    weakSelf.leftPanel.backgroundColor = [UIColor mutedOrange];
-                    weakSelf.rightPanel.layer.borderColor = [UIColor primaryOrange].CGColor;
-                    
-                    if (weakSelf.openChallenge) {
-                        weakSelf.rightPanel.backgroundColor = [UIColor primaryOrange];
-                        weakSelf.rightPanel.layer.borderWidth = 0.0f;
-                        weakSelf.challengeDescription.textColor = [UIColor whiteColor];
-                        weakSelf.challengeTitle.textColor = [UIColor whiteColor];
-                        weakSelf.challengePoints.textColor = [UIColor whiteColor];
-                    }
-                    else {
-                        weakSelf.rightPanel.backgroundColor = [UIColor clearColor];
-                        weakSelf.rightPanel.layer.borderWidth = 1.0f;
-                        weakSelf.challengeDescription.textColor = [UIColor primaryOrange];
-                        weakSelf.challengeTitle.textColor = [UIColor primaryOrange];
-                        weakSelf.challengePoints.textColor = [UIColor primaryOrange];
-                        weakSelf.separatorView.backgroundColor = [UIColor primaryOrange];
-                    }
-                    
+                if ([[PFUser currentUser][@"type"] isEqualToString:@"mentor"]) {
+                    weakSelf.activated = YES;
                 } else {
-                    weakSelf.leftPanel.backgroundColor = [UIColor mutedGreen];
-                    weakSelf.rightPanel.layer.borderColor = [UIColor primaryGreen].CGColor;
-                    
-                    if (weakSelf.openChallenge) {
-                        weakSelf.rightPanel.backgroundColor = [UIColor primaryGreen];
-                        weakSelf.rightPanel.layer.borderWidth = 0.0f;
-                        weakSelf.challengeDescription.textColor = [UIColor whiteColor];
-                        weakSelf.challengeTitle.textColor = [UIColor whiteColor];
-                        weakSelf.challengePoints.textColor = [UIColor whiteColor];
-                    }
-                    else {
-                        weakSelf.rightPanel.backgroundColor = [UIColor clearColor];
-                        weakSelf.rightPanel.layer.borderWidth = 1.0f;
-                        weakSelf.challengeDescription.textColor = [UIColor primaryGreen];
-                        weakSelf.challengeTitle.textColor = [UIColor primaryGreen];
-                        weakSelf.challengePoints.textColor = [UIColor primaryGreen];
-                        weakSelf.separatorView.backgroundColor = [UIColor primaryGreen];
-                    }
+                    weakSelf.activated = (number > 0);
                 }
+            
+                weakSelf.openChallenge = (number > 0);
+                [weakSelf configureContentViewForCurrentState];
+                
+                [MBProgressHUD hideAllHUDsForView:[UIApplication sharedApplication].keyWindow animated:YES];
             });
         }];
+
     } afterDelay:0.35f];
+}
+
+#pragma mark - Private Methods -
+- (void)configureContentViewForCurrentState
+{
+    NSString *pillar = self.challengePillarText;
+    self.challengeDescription.editable = YES;
+
+    if (self.openChallenge) {
+        self.challengeState.text = @"OPEN CHALLENGE";
+        self.challengeIcon.image = [UIImage imageNamed:@"icon_challenge_open"];
+        self.separatorView.alpha = 0.0f;
+    }
+    else {
+        self.challengeState.text = @"FUTURE CHALLENGE";
+        self.challengeIcon.image = [UIImage imageNamed:@"icon_challenge_future"];
+        self.separatorView.alpha = 1.0f;
+    }
+    
+    if ([pillar isEqualToString:@"Money Manager"]) {
+        self.leftPanel.backgroundColor = [UIColor mutedOrange];
+        self.rightPanel.layer.borderColor = [UIColor primaryOrange].CGColor;
+        
+        if (self.openChallenge) {
+            self.rightPanel.backgroundColor = [UIColor primaryOrange];
+            self.rightPanel.layer.borderWidth = 0.0f;
+            self.challengeDescription.textColor = [UIColor whiteColor];
+            self.challengeTitle.textColor = [UIColor whiteColor];
+            self.challengePoints.textColor = [UIColor whiteColor];
+        }
+        else {
+            self.rightPanel.backgroundColor = [UIColor clearColor];
+            self.rightPanel.layer.borderWidth = 1.0f;
+            self.challengeDescription.textColor = [UIColor primaryOrange];
+            self.challengeTitle.textColor = [UIColor primaryOrange];
+            self.challengePoints.textColor = [UIColor primaryOrange];
+            self.separatorView.backgroundColor = [UIColor primaryOrange];
+        }
+        
+    } else {
+        self.leftPanel.backgroundColor = [UIColor mutedGreen];
+        self.rightPanel.layer.borderColor = [UIColor primaryGreen].CGColor;
+        
+        if (self.openChallenge) {
+            self.rightPanel.backgroundColor = [UIColor primaryGreen];
+            self.rightPanel.layer.borderWidth = 0.0f;
+            self.challengeDescription.textColor = [UIColor whiteColor];
+            self.challengeTitle.textColor = [UIColor whiteColor];
+            self.challengePoints.textColor = [UIColor whiteColor];
+        }
+        else {
+            self.rightPanel.backgroundColor = [UIColor clearColor];
+            self.rightPanel.layer.borderWidth = 1.0f;
+            self.challengeDescription.textColor = [UIColor primaryGreen];
+            self.challengeTitle.textColor = [UIColor primaryGreen];
+            self.challengePoints.textColor = [UIColor primaryGreen];
+            self.separatorView.backgroundColor = [UIColor primaryGreen];
+        }
+    }
+    
+    //
+    // Weird bug that doesn't re-draw (update textColor) on UITextView unless you toggle editable
+    //  http://stackoverflow.com/questions/19113673/uitextview-setting-font-not-working-with-ios-6-on-xcode-5
+    //
+    self.challengeDescription.editable = NO;
+
+    self.challengeState.alpha = 1.0f;
+    self.rightPanel.alpha = 1.0f;
+    self.challengeIcon.alpha = 1.0f;
 }
 
 - (void)objectsReturned:(int)number error:(NSError *)error
