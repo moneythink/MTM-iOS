dd32cc8b4bf34aff08256641c11a193f8adee3dc
Colin Young <me@colinyoung.com>
2015-09-16 12:32:30 -0500

    Don't strand mentor loading profile from forced logout

--- moneythink-ios/MTEditProfileViewController.m
+++ moneythink-ios/MTEditProfileViewController.m
@@ -56,6 +56,12 @@
 {
     [super viewDidLoad];
     
+    BOOL shouldDim = YES;
+    AppDelegate *appDelegate = [MTUtil getAppDelegate];
+    if (appDelegate.logoutReason && appDelegate.logoutReason.length > 0) {
+        shouldDim = NO;
+    }
+    
     self.viewFields.delegate = self;
     
     self.isMentor = [MTUser isCurrentUserMentor];
@@ -152,7 +158,11 @@
     AppDelegate *appDelegate = [MTUtil getAppDelegate];
     if (appDelegate.logoutReason && appDelegate.logoutReason.length > 0) {
         [[[UIAlertView alloc] initWithTitle:@"Class Archived" message:appDelegate.logoutReason delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
-        [appDelegate clearLogoutReason];        
+        [appDelegate clearLogoutReason];
+
+        [self bk_performBlockInBackground:^(id obj) {
+            [MBProgressHUD hideAllHUDsForView:[UIApplication sharedApplication].keyWindow animated:YES];
+        } afterDelay:1.0];
     }
     
     [MTUtil GATrackScreen:@"Edit Profile"];
