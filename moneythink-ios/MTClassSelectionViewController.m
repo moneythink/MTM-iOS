02862008b308e101246d79a116912053b072703b
Colin Young <me@colinyoung.com>
2016-01-15 14:33:23 -0600

    Adjust time.

--- moneythink-ios/MTClassSelectionViewController.m
+++ moneythink-ios/MTClassSelectionViewController.m
@@ -459,7 +459,7 @@
             }
             
             // Dismiss after a second.
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                 [weakSelf performSegueWithIdentifier:@"dismiss" sender:nil];
             });
         }
